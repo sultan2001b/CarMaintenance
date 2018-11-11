1bccaaaa34c8491c36f0d457c447925ede6173bc
sultan2001b <sultan2001b@gmail.com>
2018-11-11 15:13:27 +0300

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
- resources :customers, :fixes, :issues 
+  root "customers#index"
+  resources :customers, :fixes, :issues
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'customers#index' 
 end
